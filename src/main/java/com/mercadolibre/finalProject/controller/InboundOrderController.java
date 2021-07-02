03a5e37f7ec79d8efaa39a1c54aef3e2ac086d03
LeonardoBatistaCariasMeli <leonardo.carias@mercadolivre.com>
2021-07-02 11:01:12 -0300

    Configuring database in application-local.yml

---
+++ src/main/java/com/mercadolibre/finalProject/controller/InboundOrderController.java
@@ -0,0 +1,12 @@
+package com.mercadolibre.finalProject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class InboundOrderController {
+
+
+
+}
