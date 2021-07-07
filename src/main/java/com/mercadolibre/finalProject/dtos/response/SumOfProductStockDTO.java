a82c21ef469c5f65819763fab3b905b4f1505e9d
Onias da Rocha <onias.rocha@mercadolivre.com>
2021-07-06 22:33:43 -0400

    creates base classes for fourth feature. pending product entity refactor to implement single product type

---
+++ src/main/java/com/mercadolibre/finalProject/dtos/response/SumOfProductStockDTO.java
@@ -0,0 +1,8 @@
+package com.mercadolibre.finalProject.dtos.response;
+
+import java.util.List;
+
+public class SumOfProductStockDTO {
+    private Long productId;
+    private List<WarehouseProductSumDTO> warehouses;
+}
---
+++ src/main/java/com/mercadolibre/finalProject/dtos/response/WarehouseProductSumDTO.java
@@ -0,0 +1,6 @@
+package com.mercadolibre.finalProject.dtos.response;
+
+public class WarehouseProductSumDTO {
+    private Long warehouseCode;
+    private Integer totalQuantity;
+}
